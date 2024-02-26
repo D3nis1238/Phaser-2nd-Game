22d7cae1b30abdc707adadb2b0a0b3822753a69f
D3nis1238 <158023075+D3nis1238@users.noreply.github.com>
2024-02-26 16:25:03 +0200

    додав гравця

--- game.js
+++ game.js
@@ -1,9 +1,30 @@
 function preload() {
-  this.load.spritesheet("dude", "assets/pixil-frame-0.png", {
-    frameWidth: 32,
-    frameHeight: 48,
+  this.load.spritesheet("dude", "assets/pixilart-drawing.png", {
+    frameWidth: 20,
+    frameHeight: 34,
   });
 } 
 function create() {
   player = this.physics.add.sprite(100, 450, "dude")
+  player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
 }
