cea0a5ea9a089844f4ae4f1388b4e2d098f07281
D3nis1238 <158023075+D3nis1238@users.noreply.github.com>
2024-03-04 16:28:22 +0200

    щось зробив

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
-  type: Phaser.AUTO,   
+  type: Phaser.AUTO,
   width: 1920,
-  height: 1080, 
+  height: 1080,
   physics: {
     default: "arcade",
     arcade: {
@@ -24,23 +24,39 @@ var lives = 3;
 var worldwidth = 9600;
 
 function preload() {
-  this.load.image("fon", "assets/fon.png");
- // this.load.image("fon1", "assets/фон+.jpg");
-    this.load.image("platform", "assets/platform.png");
-    this.load.image("star", "assets/star-1.png");
-    this.load.image("skelet", "assets/chaxluknevmuryshiu.png");
-    this.load.image("tree", "assets/Tree_2.png");
-    this.load.image("stone", "assets/Stone.png");
-    this.load.image("bush", "assets/Bush (3).png");
+  this.load.image("fon", "assets/igra_tsvetov_raznye_tsveta_linii_51895_1920x1080.jpg");
+  // this.load.image("fon1", "assets/фон+.jpg");
+  this.load.image("platform", "assets/platform.png");
+  this.load.image("star", "assets/star-1.png");
+  this.load.image("skelet", "assets/chaxluknevmuryshiu.png");
+  this.load.image("tree", "assets/Tree_2.png");
+  this.load.image("stone", "assets/Stone.png");
+  this.load.image("bush", "assets/Bush (1).png");
   this.load.spritesheet("dude", "assets/pixilart-drawing.png", {
     frameWidth: 20,
     frameHeight: 34,
   });
-} 
+}
+
 function create() {
+  //
+  this.add.tileSprite(0, 0, worldwidth, 1080, "fon").setOrigin(0, 0);
+
+
+  //
+  platforms = this.physics.add.staticGroup();
+
+  for (var x = 0; x < worldwidth; x = x + 1120) {
+    console.log(x);
+    platforms.create(x, 776, "platform").setOrigin(0, 0).refreshBody();
+  }
+
+
+  //
   player = this.physics.add.sprite(100, 450, "dude")
   player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+  player.setCollideWorldBounds(true);
+  //
   this.anims.create({
     key: "left",
     frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
@@ -59,35 +75,35 @@ function create() {
     frameRate: 10,
     repeat: -1,
   });
-this.add.tileSprite(0,0,worldwidth, 1080,"fon").setOrigin(0,0);
-platforms = this.physics.add.staticgroup();
-for (var x = 0; x < worldwidth; x = x + 1120) {
-  console.log(x);
-  plstform.create(x,1000, "ground").setOrigin(0, 0).refreshBody();
+  //
+  cursors = this.input.keyboard.createCursorKeys();
+
+
+
+
+  this.cameras.main.setBounds(0, 0, worldwidth, window.innerHeight);
+  this.physics.world.setBounds(0, 0, worldwidth, window.innerHeight);
+  this.cameras.main.startFollow(player);
 }
-this.camera.main.setBounds(0,0,worldwidth, window.innerHeight);
-this.camera.world.setBounds(0,0,worldwidth, window.innerHeight);
-this.camera.main.startFollow(dude);
-}
-function upgrade(){
-  platforms = this.physics.add.staticGroup(); // Це створює нову групу статичної фізики та призначає її локальним змінним платформам
-    if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-  
-      player.anims.play("left", true);
-    } 
-    else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-  
-      player.anims.play("right", true);
-    } 
-    else {
-      player.setVelocityX(0);
-  
-      player.anims.play("turn");
-    }
-    if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
-    }
+function update() {
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play("left", true);
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  }
+  else {
+    player.setVelocityX(0);
+
+    player.anims.play("turn");
+  }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 
 }
